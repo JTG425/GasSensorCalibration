557dd2d791c86f73f58f9f350264a524273b4845
JTG425 <golonka.10@buckeyemail.osu.edu>
2024-01-23 17:13:46 -0500

    Updates

--- eventLog.py
+++ eventLog.py
@@ -1,1 +1,10 @@
-# Log Previous Runs of the System in a File
+# This File will Generate the event log gui
+# It will use the events.txt file to parse through
+# previous events and display them in a list
+# when an event is clicked, the graph of that event
+# will be displayed in the graph window.
+# must contain a back button to return to the main calibration mode.
+# Look at events.txt for the format of the file read in.
+
+
+# This file might also be responsible for writing to the file, but that is not yet determined.
