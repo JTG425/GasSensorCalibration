231dda215640214e49449aa2193b7a3a30043c97
JTG425 <golonka.10@buckeyemail.osu.edu>
2024-02-29 10:19:14 -0500

    Styling

--- graph.py
+++ graph.py
@@ -1,20 +1,18 @@
 import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QFrame
+from PyQt6.QtCore import Qt, QTimer
+from PyQt6.QtGui import QFont
 import pyqtgraph as pg
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QScrollArea, QFrame, QGridLayout
-from PyQt6.QtCore import Qt, pyqtSignal, QTimer
-from PyQt6.QtGui import QColor, QPalette, QShowEvent
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 class MakeGraph(QWidget):
-    graphBackground = "#f9fafc"
+    graphBackground = "#232629"
     graphLine = "#063e78"
     borders = "#3b4045"
     
     time = {}  # Time points
     data = {}  # Data points
-    eventTime = {} # Event Time Points
-    eventData = {} # Event Data Points
+    eventTime = {}  # Event Time Points
+    eventData = {}  # Event Data Points
     
     def __init__(self, parent=None):
         super(MakeGraph, self).__init__(parent)
@@ -22,24 +20,37 @@ class MakeGraph(QWidget):
         self.showLive = True
         self.eventDataValues = {}
         self.eventDateValue = None
-        self.gridLayout = QGridLayout
         self.counter = 0
         self.eventCounter = 0
         self.initUI()
         
     def initUI(self):
-        layout = QVBoxLayout(self)
+        layout = QVBoxLayout(self)  # Main layout
+        self.graphFrame = QFrame()
+        self.graphFrame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.graphFrame.setFrameShadow(QFrame.Shadow.Raised)
+        self.graphFrame.setStyleSheet(
+            f"background-color: {self.graphBackground};"
+            f"border: 1px solid {self.borders};"
+            "padding: 10px;"
+        )
+    
+        # Create a layout for the graphFrame
+        graphFrameLayout = QVBoxLayout(self.graphFrame)  
+        self.graphFrame.setLayout(graphFrameLayout)  # Set the layout for graphFrame
 
-        self.figure = Figure()
-        self.canvas = FigureCanvas(self.figure)
-        layout.addWidget(self.canvas)
+        self.graphWidget = pg.PlotWidget()
+        graphFrameLayout.addWidget(self.graphWidget)  # Add the graphWidget to the graphFrame's layout
 
-        self.ax = self.figure.add_subplot(111)
-        self.figure.patch.set_facecolor(self.graphBackground)
-        self.ax.set_facecolor(self.graphBackground)
-        self.ax.set_title('Concentration In PPM')
-        self.ax.set_xlabel('Time')
-        self.ax.set_ylabel('PPM')
+        # Now the graphWidget is within the graphFrame, you can set up the graphWidget as before
+        self.graphWidget.setBackground(self.graphBackground)
+        self.graphWidget.setTitle('Concentration In PPM', color='k')
+        self.graphWidget.setLabel('left', 'PPM')
+        self.graphWidget.setLabel('bottom', 'Time')
+        self.graphWidget.getPlotItem().getAxis('bottom').setPen(self.graphLine)
+        self.graphWidget.getPlotItem().getAxis('left').setPen(self.graphLine)
+
+        layout.addWidget(self.graphFrame)  # Add the graphFrame to the main layout
 
         self.tempLiveData()
         
@@ -50,7 +61,6 @@ class MakeGraph(QWidget):
         
         self.showLiveGraph()
         
-
     def showLiveGraph(self):
         self.timer = QTimer(self)  # QTimer to update the graph
         self.timer.timeout.connect(self.updateLiveGraph)
@@ -63,16 +73,12 @@ class MakeGraph(QWidget):
             if self.counter < len(self.time):
                 time_keys = list(self.time.keys())[:self.counter + 1]
                 data_values = [self.data[key] for key in time_keys]
-                self.ax.plot(time_keys, data_values, '-o', color=self.graphLine)
-                self.canvas.draw()  # Update the canvas with the new plot
+                self.graphWidget.plot(time_keys, data_values, pen=pg.mkPen(self.graphLine, width=2))
                 self.counter += 1
             else:
                 self.timer.stop()  # Stop the timer if all points are plotted
         else:
             return
-
-            
-            
 
     def setEventTime(self, time):
         self.eventTimeValue = time
@@ -85,14 +91,11 @@ class MakeGraph(QWidget):
 
     def showEventLog(self):
         self.showLive = False
-        self.dataReady = False
         self.timer.stop()
-        self.ax.clear()
+        self.graphWidget.clear()
         self.counter = 0
         self.eventCounter = 0
-        self.ax.set_title(f'Calibration Performed On {self.eventDateValue}')
-        self.ax.set_xlabel('Time')
-        self.ax.set_ylabel('PPM')
+        self.graphWidget.setTitle(f'Calibration Performed On {self.eventDateValue}')
         
         if self.eventTimeValue is None or self.eventDataValues == [] or self.eventDateValue is None:
             print("No data to show")
@@ -101,19 +104,17 @@ class MakeGraph(QWidget):
                 self.eventTime[i] = i
                 self.eventData[i] = self.eventDataValues[i]   
             print(self.eventData)
-            self.dataReady = True
-            if self.dataReady:
-                self.eventTimer = QTimer(self)  # QTimer to update the graph
-                self.eventTimer.timeout.connect(self.updateEventGraph)
-                self.eventTimer.start(100)  # Update The Plot every 10
-                print(f"{self.eventDateValue} Data Plotting...")
+            self.eventTimer = QTimer(self)  # QTimer to update the graph
+            self.eventTimer.timeout.connect(self.updateEventGraph)
+            self.eventTimer.start(100)  # Update The Plot every 10
+            print(f"{self.eventDateValue} Data Plotting...")
             
     def updateEventGraph(self):
         if self.eventCounter < len(self.eventTime):
             time_keys2 = list(self.eventTime.keys())[:self.eventCounter + 1]
             data_values2 = [self.eventData[key] for key in time_keys2]
-            self.ax.plot(time_keys2, data_values2, '-o', color=self.graphLine)
-            self.canvas.draw()  # Update the canvas with the new plot                
+            self.graphWidget.plot(time_keys2, data_values2, pen=pg.mkPen(self.graphLine, width=2))
             self.eventCounter += 1
         else:
-            self.timer.stop()  # Stop the timer if all points are plotted
+            self.eventTimer.stop()  # Stop the timer if all points are plotted
+
